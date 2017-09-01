913678e2e3888b609ac5f8c295bc8fbbb397eee7
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-09-01 23:45:58 +1000

    add broken diamond impl

--- src/main/scala/net/ssanj/intro/pbt/DiamondBroken.scala
+++ src/main/scala/net/ssanj/intro/pbt/DiamondBroken.scala
@@ -1,14 +1,16 @@
 package net.ssanj.intro.pbt
 
 /**
- * http://claysnow.co.uk/recycling-tests-in-tdd/
+ * Various breakages
  */
 
-trait Diamond {
+trait DiamondBroken {
 
   private val start = 'A'
 
   def instances(supplied: Char): Int = if (supplied == start) 1 else 2
+
+//  def instances(supplied: Char): Int = if (supplied == start) 1 else 3 //add extra instances
 
   def betweenSpace(supplied: Char): Int = {
     val diff = supplied - start
@@ -24,17 +26,20 @@ trait Diamond {
     val outside = " " * outerSpace(supplied, current)
     val values  = (1 to instances(current)).map(_ => current)
 
+    // values.mkString(outside, inside, "") //remove inside space
+    // values.mkString(outside, "", outside) //remove outside space
     values.mkString(outside, inside, outside)
   }
 
   def printDiamond(supplied: Char): String = {
     if (supplied == start || !('A' to 'Z').contains(supplied)) start.toString
-    else {
+    else /*if (('B' to 'T').contains(supplied))*/ { //sub range
       val chars  = previousChars(supplied) //A
       val before = chars.foldLeft(Seq.empty[String])((acc, c) => acc :+ printChar(supplied, c))
       val after  = before.reverse
+      // val after  = before.reverse //don't reverse
       val line   = printChar(supplied, supplied)
       (before ++ (line +: after)).mkString("\n")
-    }
+    } //else { "A\nA" } //return default
   }
 }
