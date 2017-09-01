2c73be5af9f6f4726788f2a498ea90ea3edca481
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-09-01 23:12:04 +1000

    add more props

--- src/test/scala/net/ssanj/intro/pbt/DiamondProps.scala
+++ src/test/scala/net/ssanj/intro/pbt/DiamondProps.scala
@@ -6,16 +6,18 @@ import org.scalacheck.Prop._
 /**
  * What the properties of a Diamond?
  *
- * 1. Given a letter, the diamond will start with 'A'
- * 2. Given a letter, the diamond will end with 'A'
- * 3. The widest point will only have the supplied letter and spaces
- * 4. Characters will be listed in ascending order until given letter and then descend
- * 5. The lines above the widest line will be a mirror image of the lines below it
- * 6. Each line should have two spaces more in-between chars than the line above it until widest line
- * 7. Each line should have two space on the less outside chars than the line above it
- * 8. All unhandled inputs should return 'A'
- * 9. The number of lines should be the length of the first line
+ * 1. Given any letter, the diamond will start with 'A'
+ * 2. Given a valid letter, the diamond will end with 'A'
+ * 3. The widest point will only have the supplied letter and spaces in between
+ * 4. Any line but the first and last will only have one repeated letter and spaces
+ * 5. Characters will be listed in ascending order until given letter
+ * 6. The lines above the widest line will be a mirror image of the lines below it
+ * 7. Each line should have two spaces more in-between chars than the line above it until widest line
+ * 8. Each line should have two space on the less outside chars than the line above it
+ * 9. The number of lines should be the length of the any line
  */
+
+//TODO: Move common code out.
 
 object DiamondProps extends Properties("Diamond") with Diamond {
 
@@ -34,18 +36,42 @@ object DiamondProps extends Properties("Diamond") with Diamond {
       printDiamond(ch).split("\n").last.contains("A")
     }
 
-  property("The widest point will only have the supplied letter and spaces") =
-    forAll(genCharWithoutA) { uc: UpperCharWithoutA =>
-      val ch = uc.value
+  property("The widest point will only have the supplied letter and spaces in between") =
+    forAll(genCharWithoutA) { ucwa: UpperCharWithoutA =>
+      val ch = ucwa.value
       val lines = printDiamond(ch).split("\n")
       val ((wline, _), windex) = lines.map(l => (l, l.trim.length)).zipWithIndex.maxBy(_._1._2)
 
       ((wline.count(_ == ch) ?= 2) :| s"expected 2 [$ch] but got [$wline]") && wline.filterNot(_ == ch).trim.isEmpty :| s"expected only spaces other than [$ch] but got [$wline]"
     }
 
-  property("The lines above the widest line will be a mirror image of the lines below it") =
+  property("Any line but the first and last will only have one repeated letter and spaces") =
+    forAll(genCharWithoutA) { ucwa: UpperCharWithoutA =>
+      val ch = ucwa.value
+      val lines = printDiamond(ch).split("\n")
+      val remaining = lines.tail.init //drop 'A's
+      val rangeWithoutA = ('B' to 'Z')
+
+      remaining.forall { l =>
+        val unique = l.toSet
+        l.filterNot(_ == ' ').length == 2 &&
+        unique.size == 2 &&
+        unique.contains(' ') &&
+        unique.exists(rangeWithoutA.contains)
+      }
+    }
+
+  property("Characters will be listed in ascending order until given letter") =
     forAll(genCharWithoutA) { uc: UpperCharWithoutA =>
       val ch = uc.value
+      val lines = printDiamond(ch).split("\n")
+      val uniqueChars = lines.map(_.filterNot(_ == ' ').head).slice(0, lines.length / 2)
+      uniqueChars.zip(uniqueChars.tail).forall { case (c1, c2) => c1 < c2 }
+    }
+
+  property("The lines above the widest line will be a mirror image of the lines below it") =
+    forAll(genCharWithoutA) { ucwa: UpperCharWithoutA =>
+      val ch = ucwa.value
       val lines = printDiamond(ch).split("\n")
       val (_, windex) = lines.map(_.trim.length).zipWithIndex.maxBy(_._1)
       val before = lines.slice(0, windex).toList
@@ -54,8 +80,8 @@ object DiamondProps extends Properties("Diamond") with Diamond {
     }
 
   property("Each line should have two spaces more in-between chars than the line above it until widest line") =
-    forAll(genCharWithoutA) { uc: UpperCharWithoutA =>
-      val ch     = uc.value
+    forAll(genCharWithoutA) { ucwa: UpperCharWithoutA =>
+      val ch     = ucwa.value
       val lines  = printDiamond(ch).split("\n").map(_.trim)
       val mid    = lines.length / 2 //always odd
       val above  = lines.slice(0, mid + 1)
